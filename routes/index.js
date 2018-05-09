4863da2a8398145ac83a1f049f1763142ee0bab0
Carlos Gurucharri Aristi <c.gurucharria@alumnos.upm.es>
2018-05-09 13:16:36 +0200

    p5 commit

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+const Sequelize = require('sequelize');
+const sequelize = require('../models/index');
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'P5_Quiz' });
+});
+
+/* GET credits page. */
+router.get('/credits', function(req, res, next) {
+  res.render('credits', { title: 'Creditos' });
+});
+
+router.get('/quizzes', function(req, res, next) {
+
+  sequelize.models.quiz.findAll()
+      .then(quizzes => {
+        res.render('quizzes', {quizzes});
+  })
+  .catch(error =>{
+    next(error);
+  });
+});
+
+module.exports = router;
